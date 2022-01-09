618115290333769cdbfb5f457ed9b614fc67afcc
Lenin <lerama@floridauniversitaria.es>
2022-01-09 20:00:10 +0100

    Cambios para resolver el bug_ej3.

--- indice.js
+++ indice.js
@@ -1,1 +1,3 @@
-Cambios para resolver el bug_ej2, diferentes a los realizados en la funcionalidad.
+Cambios para resolver el bug_ej2, diferentes a los realizados en la funcionalidad..
+
+Nuevos cambios para resolver el bug_ej3.
